d2fe6a73e68077f741f975942a0b9486fd486ec6
Ben Kogan <benkogan9@gmail.com>
2023-06-27 23:45:30 -0400

    2, plus keep things in main.go

---
+++ cmd/2/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func readInput(day string, name string) string {
+	pwd, _ := os.Getwd()
+	path := fmt.Sprintf("%s/cmd/%s/%s.txt", pwd, day, name)
+	b, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Print(err)
+	}
+	return string(b)
+}
+
+func main() {
+	day := os.Args[1]
+	var inputFileName string
+	if len(os.Args) > 2 && os.Args[2] == "test" {
+		inputFileName = "test"
+	} else {
+		inputFileName = "in"
+	}
+	input := readInput(day, inputFileName)
+
+	fmt.Printf("Part 1: %v\n", part1(input))
+	fmt.Printf("Part 2: %v", part2(input))
+}
+
+type Validation struct {
+	low      int
+	high     int
+	letter   string
+	password string
+}
+
+func validationFromString(line string) Validation {
+	split := strings.Split(line, " ")
+	rangeStr := strings.Split(split[0], "-")
+	low, _ := strconv.Atoi(string(rangeStr[0]))
+	high, _ := strconv.Atoi(string(rangeStr[1]))
+	return Validation{
+		low:      low,
+		high:     high,
+		letter:   string(split[1][0]),
+		password: split[2],
+	}
+}
+
+func (v *Validation) validate() bool {
+	count := strings.Count(v.password, v.letter)
+	return count >= v.low && count <= v.high
+}
+
+func (v *Validation) tobogganValidate() bool {
+	first := string(v.password[v.low-1]) == v.letter
+	second := string(v.password[v.high-1]) == v.letter
+	return first != second
+}
+
+func part1(input string) int {
+	total := 0
+	for _, line := range strings.Split(input, "\n") {
+		validation := validationFromString(line)
+		if validation.validate() {
+			total++
+		}
+	}
+	return total
+}
+
+func part2(input string) int {
+	total := 0
+	for _, line := range strings.Split(input, "\n") {
+		validation := validationFromString(line)
+		if validation.tobogganValidate() {
+			total++
+		}
+	}
+	return total
+}
