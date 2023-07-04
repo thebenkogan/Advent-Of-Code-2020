79c12b722356347e786b1f55711accc6726be533
Ben Kogan <benkogan9@gmail.com>
2023-07-04 05:37:48 -0400

    7

---
+++ cmd/7/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/zyedidia/generic/stack"
+)
+
+func main() {
+	day := os.Args[1]
+	var inputFileName string
+	if len(os.Args) > 2 && os.Args[2] == "test" {
+		inputFileName = "test"
+	} else {
+		inputFileName = "in"
+	}
+	pwd, _ := os.Getwd()
+	path := fmt.Sprintf("%s/cmd/%s/%s.txt", pwd, day, inputFileName)
+	b, _ := os.ReadFile(path)
+	input := string(b)
+
+	fmt.Printf("Part 1: %v\n", part1(input))
+	fmt.Printf("Part 2: %v", part2(input))
+}
+
+func countOuterBags(adj map[string][]string, start string) int {
+	total := 0
+	stack := stack.New[string]()
+	visited := make(map[string]bool)
+	stack.Push(start)
+	for stack.Size() > 0 {
+		next := stack.Pop()
+		for _, neighbor := range adj[next] {
+			if !visited[neighbor] {
+				stack.Push(neighbor)
+				visited[neighbor] = true
+				total++
+			}
+		}
+	}
+	return total
+}
+
+func part1(input string) int {
+	adj := make(map[string][]string)
+	containRegex, _ := regexp.Compile(`\d+ (\w+ \w+) bags?`)
+	for _, rule := range strings.Split(input, "\n") {
+		id := strings.Split(rule, " bags")[0]
+		contains := containRegex.FindAllStringSubmatch(rule, -1)
+		for _, contain := range contains {
+			adj[contain[1]] = append(adj[contain[1]], id)
+		}
+	}
+	return countOuterBags(adj, "shiny gold")
+}
+
+type Bag struct {
+	id    string
+	count int
+}
+
+func countInnerBags(adj map[string][]Bag, start string) int {
+	total := 0
+	stack := stack.New[Bag]()
+	stack.Push(Bag{id: start, count: 1})
+	for stack.Size() > 0 {
+		next := stack.Pop()
+		if neighbors, ok := adj[next.id]; ok {
+			total += next.count
+			for _, neighbor := range neighbors {
+				stack.Push(Bag{id: neighbor.id, count: neighbor.count * next.count})
+			}
+		} else {
+			total += next.count
+		}
+	}
+	return total - 1 // subtract out start bag
+}
+
+func part2(input string) int {
+	adj := make(map[string][]Bag)
+	containRegex, _ := regexp.Compile(`(\d+) (\w+ \w+) bags?`)
+	for _, rule := range strings.Split(input, "\n") {
+		id := strings.Split(rule, " bags")[0]
+		contains := containRegex.FindAllStringSubmatch(rule, -1)
+		for _, contain := range contains {
+			count, _ := strconv.Atoi(contain[1])
+			adj[id] = append(adj[id], Bag{id: contain[2], count: count})
+		}
+	}
+
+	return countInnerBags(adj, "shiny gold")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module aoc2020
 
 go 1.20
+
+require github.com/zyedidia/generic v1.2.1
